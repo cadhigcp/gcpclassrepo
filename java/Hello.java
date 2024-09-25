3af776b2a2a45a624db60cb87e9fb9ecce6a60eb
cadhigcp <cadhimulam@gmail.com>
2024-09-25 20:13:14 +0530

    java file is updated

--- java/Hello.java
+++ java/Hello.java
@@ -2,5 +2,10 @@ public class Hello
 {
 	public static void main(String []Args){
 		System.out.println("Hello Java");
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println("the sum is "+c);
 	}
 }
